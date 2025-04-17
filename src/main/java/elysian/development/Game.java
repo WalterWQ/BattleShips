9317f61eb1eee53b6537aff1e17c269e06277c64
Carter Morrison <40690697@live.napier.ac.uk>
2025-04-17 22:06:32 +0100

    Added initial menu system and user input and validation Utils

--- src/main/java/elysian/development/Game.java
+++ src/main/java/elysian/development/Game.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Game {
 
     Scanner userInput = new Scanner(System.in);
+    static int selectedDifficulty = 0;
+    static int selectedClass = 0;
 
     public static void StartMenu(){
         Scanner userInput = new Scanner(System.in);
@@ -19,7 +21,24 @@ public class Game {
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
         userInput.next();
 
-        Utils.validationChecker(userInput);
+        selectedDifficulty = Utils.getValidChoice(new int[]{1, 2, 3, 4}, userInput);
+
+        System.out.println("-=-=-=SELECT A CLASS-=-=-=");
+        System.out.println("-=-=-=** CLASSES OFFER DIFFERENT SHIPS OR DIFFERENT FEATURES SUCH AS SMALLER BOATS **-=-=-=");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("-=-=-=TYPE 1 FOR STEALTH BOATS-=-=-=");
+        System.out.println("-=-=-=TYPE 2 FOR HEAVY DUTY BOATS-=-=-=");
+        System.out.println("-=-=-=TYPE 3 FOR FIGHTER-=-=-=");
+        System.out.println("-=-=-=TYPE 4 FOR AIR DEFENSE-=-=-=");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
+        userInput.next();
+
+        selectedClass = Utils.getValidChoice(new int[]{1, 2, 3, 4}, userInput);
+
+        System.out.println("-=-=-=LOADING GAME-=-=-=");
+
+        StartGame(selectedDifficulty, selectedClass);
+
 
     }
 }
--- src/main/java/elysian/development/Utils.java
+++ src/main/java/elysian/development/Utils.java
@@ -1,10 +1,28 @@
 package elysian.development;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Utils {
 
-    public static boolean validationChecker(Scanner userInput, int expectedInput){
+    static int getValidChoice(int[] validOptions, Scanner scanner) {
+        while (true) {
+            try {
+                System.out.print("Enter your choice: ");
+                int input = Integer.parseInt(scanner.next());
+                for (int option : validOptions) {
+                    if (input == option) {
+                        return input;
+                    }
+                }
+                System.out.println("Invalid choice. Please enter one of: " + Arrays.toString(validOptions));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+            }
+        }
+    }
 
+    public static boolean isValidGridCoord(int row, int col, char[][] grid) {
+        return row >= 0 && row < grid.length && col >= 0 && col < grid[0].length;
     }
 }
